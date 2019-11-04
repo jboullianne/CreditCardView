b0913e923e50ac76478db2f610974f890521f821
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-04 17:29:50 -0500

    Added CCBackgroundView to CreditCardView

--- CreditCardView.swift
+++ CreditCardView.swift
@@ -9,12 +9,44 @@ import UIKit
 
 class CreditCardView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var backgroundView:CCBackgroundView
+    
+    
+    override init(frame: CGRect) {
+        backgroundView = CCBackgroundView(frame: frame)
+        
+        super.init(frame: frame)
+        
+        setupViews()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        backgroundView = CCBackgroundView()
+        
+        super.init(coder: aDecoder)
+        
+        setupViews()
+    }
+    
+    init(frame: CGRect, template: CCBackgroundView.CCBackgroundTemplate) {
+        backgroundView = CCBackgroundView(frame: frame, template: template)
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    func setupViews() {
+        self.addSubview(backgroundView)
+    }
+    
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
 
 }

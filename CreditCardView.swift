215c96ffde319e25e933649e9190c02d6c863772
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-04 17:11:49 -0500

    Created templates for Credit Card Classes

---
+++ CreditCardView.swift
@@ -0,0 +1,20 @@
+//
+//  CreditCardView.swift
+//  CreditCardView
+//
+//  Created by Jean-Marc Boullianne on 11/4/19.
+//
+
+import UIKit
+
+class CreditCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

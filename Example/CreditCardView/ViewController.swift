7508996db83713bab4c693fc6bdf4239c30cfefa
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-08 16:51:31 -0500

    Added CALayer Extension and Shadow to Example Credit Card.

--- Example/CreditCardView/ViewController.swift
+++ Example/CreditCardView/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         creditCardView = CreditCardView(frame: CGRect(x: 10, y: 70, width: self.view.frame.width - 20, height: 200), template: .Basic)
         self.view.addSubview(creditCardView)
+        
+        // Add Shadow to Credit card
+        creditCardView.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension CALayer {
+    func applySketchShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0)
+    {
+        shadowColor = color.cgColor
+        shadowOpacity = alpha
+        shadowOffset = CGSize(width: x, height: y)
+        shadowRadius = blur / 2.0
+        if spread == 0 {
+            shadowPath = nil
+        } else {
+            let dx = -spread
+            let rect = bounds.insetBy(dx: dx, dy: dx)
+            shadowPath = UIBezierPath(rect: rect).cgPath
+        }
+    }
 }
 

e403204ad4a9cf1a614189d0088138df093a43a8
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-08 16:58:37 -0500

    Created CCContentView Class and added to CreditCardView.

---
+++ CCContentView.swift
@@ -0,0 +1,20 @@
+//
+//  CCContentView.swift
+//  CreditCardView
+//
+//  Created by Jean-Marc Boullianne on 11/8/19.
+//
+
+import UIKit
+
+class CCContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CreditCardView.swift
+++ CreditCardView.swift
@@ -11,21 +11,27 @@ public class CreditCardView: UIView {
 
     
     var backgroundView:CCBackgroundView
+    var cardContentView:CCContentView
+    
+    var CONTENT_PADDING:CGFloat = 15
     
     public override init(frame: CGRect) {
         backgroundView = CCBackgroundView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+        cardContentView = CCContentView(frame: CGRect(x: CONTENT_PADDING, y: CONTENT_PADDING, width: frame.width - (CONTENT_PADDING * 2), height: frame.height - (CONTENT_PADDING * 2)))
         super.init(frame: frame)
         setupViews()
     }
     
     public required init?(coder aDecoder: NSCoder) {
         backgroundView = CCBackgroundView()
+        cardContentView = CCContentView()
         super.init(coder: aDecoder)
         setupViews()
     }
     
     public init(frame: CGRect, template: CCBackgroundView.CCBackgroundTemplate) {
         backgroundView = CCBackgroundView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height), template: template)
+        cardContentView = CCContentView(frame: CGRect(x: CONTENT_PADDING, y: CONTENT_PADDING, width: frame.width - (CONTENT_PADDING * 2), height: frame.height - (CONTENT_PADDING * 2)))
         super.init(frame: frame)
         setupViews()
     }
@@ -33,6 +39,8 @@ public class CreditCardView: UIView {
     func setupViews() {
         self.addSubview(backgroundView)
         backgroundView.layer.cornerRadius = 10.0
+        
+        self.addSubview(cardContentView)
     }
     
     

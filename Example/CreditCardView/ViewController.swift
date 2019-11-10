00a05053433fbecad4eb33adbc2205f85e824b54
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-09 19:11:08 -0500

    Modified Example to show all four templates on 11-inch iPad Simulator.

--- Example/CreditCardView/ViewController.swift
+++ Example/CreditCardView/ViewController.swift
@@ -11,43 +11,70 @@ import CreditCardView
 
 class ViewController: UIViewController {
 
-    var creditCardView:CreditCardView!
+    var card1:CreditCardView!
+    var card2:CreditCardView!
+    var card3:CreditCardView!
+    var card4:CreditCardView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let c1:UIColor = UIColor(rgb: 0xFBBC7C)
-        let c2:UIColor = UIColor(rgb: 0xFA8D58)
-        let c3:UIColor = UIColor(rgb: 0xFA7574)
-        let c4:UIColor = UIColor(rgb: 0xFBA595)
-        let c5:UIColor = UIColor(rgb: 0x7C58FB)
+        let c1:UIColor = UIColor(rgb: 0x7CEA9C)
+        let c2:UIColor = UIColor(rgb: 0x2FB799)
+        let c3:UIColor = UIColor(rgb: 0x454851)
+        let c4:UIColor = UIColor(rgb: 0x6F73D2)
+        let c5:UIColor = UIColor(rgb: 0x98C1D9)
         
         // Examples of Different Templates
-        /*
-        creditCardView = CreditCardView(frame: CGRect(x: 10, y: 50, width: self.view.frame.width - 20, height: 215),
+        
+        
+        card1 = CreditCardView(frame: CGRect(x: (self.view.frame.width - 880)/2, y: (self.view.frame.height - 530)/2, width: 390, height: 215),
+                               template: .Basic(c1, c2, c3))
+        
+        card2 = CreditCardView(frame: CGRect(x: self.view.frame.width - ((self.view.frame.width - 880)/2) - 390, y: (self.view.frame.height - 530)/2, width: 390, height: 215),
+                               template: .Curve(c1, c2, c3, c4, c5))
+        
+        card3 = CreditCardView(frame: CGRect(x: (self.view.frame.width - 880)/2, y: self.view.frame.height - ((self.view.frame.height - 530)/2) - 215, width: 390, height: 215),
                                         template: .Gradient(c1, c2))
         
-        creditCardView = CreditCardView(frame: CGRect(x: 10, y: 50, width: self.view.frame.width - 20, height: 215),
-                                        template: .Basic(c1, c2, c3))
- 
-        creditCardView = CreditCardView(frame: CGRect(x: 10, y: 50, width: self.view.frame.width - 20, height: 215),
+        card4 = CreditCardView(frame: CGRect(x: self.view.frame.width - ((self.view.frame.width - 880)/2) - 390, y: self.view.frame.height - ((self.view.frame.height - 530)/2) - 215, width: 390, height: 215),
                                         template: .Flat(c3))
-        */
         
-        creditCardView = CreditCardView(frame: CGRect(x: 10, y: 50, width: self.view.frame.width - 20, height: 215),
-                                        template: .Curve(c1, c2, c3, c4, c5))
-        
-        self.view.addSubview(creditCardView)
+        self.view.addSubview(card1)
+        self.view.addSubview(card2)
+        self.view.addSubview(card3)
+        self.view.addSubview(card4)
         
         // Add Shadow to Credit card
-        creditCardView.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
+        card1.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
+        card2.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
+        card3.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
+        card4.layer.applySketchShadow(color: UIColor.black, alpha: 0.5, x: 0, y: 2, blur: 60, spread: 0)
         
-        // Changeg Card Details
-        creditCardView.nameLabel.text = "Jean-Marc Boullianne"
-        creditCardView.expLabel.text = "02/30"
-        creditCardView.numLabel.text = "1111 2222 3333 4444"
-        creditCardView.brandLabel.text = "VISA"
-        creditCardView.brandImageView.image = UIImage()
+        // Change Card Details
+        card1.nameLabel.text = "Jean-Marc Boullianne"
+        card1.expLabel.text = "02/30"
+        card1.numLabel.text = "1111 2222 3333 4444"
+        card1.brandLabel.text = "VISA"
+        card1.brandImageView.image = UIImage()
+        
+        card2.nameLabel.text = "Jean-Marc Boullianne"
+        card2.expLabel.text = "02/30"
+        card2.numLabel.text = "1111 2222 3333 4444"
+        card2.brandLabel.text = "VISA"
+        card2.brandImageView.image = UIImage()
+        
+        card3.nameLabel.text = "Jean-Marc Boullianne"
+        card3.expLabel.text = "02/30"
+        card3.numLabel.text = "1111 2222 3333 4444"
+        card3.brandLabel.text = "VISA"
+        card3.brandImageView.image = UIImage()
+        
+        card4.nameLabel.text = "Jean-Marc Boullianne"
+        card4.expLabel.text = "02/30"
+        card4.numLabel.text = "1111 2222 3333 4444"
+        card4.brandLabel.text = "VISA"
+        card4.brandImageView.image = UIImage()
     }
 
     override func didReceiveMemoryWarning() {
